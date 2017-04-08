04296abcbaa1acb4bdf0bb87173b9a07b7183db7
Hack Reactor Student <student@hackreactor.com>
2017-04-08 16:51:52 -0700

    Completed Bare Minimum

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -18,13 +18,59 @@ $(document).ready(function(){
 		//get the messages for the specific room name 
 	
   });
+
+  $(".friends-menu").click('.friendlink', function(event){
+		//store the exact room that was clicked 
+		console.log("hi");
+		clearMessages(); 
+		var selectedRoom= event.toElement.innerHTML;
+		event.preventDefault(); 
+			console.log(typeof(selectedRoom));
+
+		if (!friends.includes(selectedRoom)) {
+				filterMessages(selectedRoom);
+		}
+
+		document.execCommand('bold');
+	  username.style.fontWeight = 'bold';
+		//$('small').css('font-weight','bold');
+		//$(".friendlink").toggleClass("bold");
+		//turn .friendLink bold 
+		//$('.friends-menu').css("font-weight","Bold");
+
+
+	
+
+
+		
+		
+		//get the messages for the specific room name 
+	
+  });
+
+	//need to add a different class name for usernames because chats references the messages too 
+
+  $("#chats").click('.links', function(event){
+		//store the exact room that was clicked
+		var selectedUser= event.toElement.innerHTML;
+		event.preventDefault(); 
+		addFriend(selectedUser);
+		console.log('hi')
+
+
+		$('#chats small').css("font-weight","Bold");
+		
+		//get the messages for the specific room name 
+	
+  });
 });
+
 
 
 //now we need to create the message panels
 var data; 
 var ajaxPost;
-
+var friends = [];
 
 //declares a variable data in the global scope, TO BE ASSIGNED in the "success: function (data)"******
 //so that we can assign the server data to the global scope 
@@ -82,6 +128,27 @@ var ajaxFetch = function() {
 // displays those usernames as friendslist
 // once the user clicks  and adds a friend, filter the messages from that friend and dispaly to user.
 
+function addFriend(user) { //creating friends list, can create a dropdown menu/list/button
+
+	friends.push(user); 
+	//if the person clicked was user, add to friends list, create a div and append the friends as they're clicked 
+	//somehow return the person that was clicked using the HTML thing 
+
+	//user is the person that was clicked
+	//friends.push(user); 
+
+	//wrap in button 
+	friends= _.uniq(friends);
+
+
+	console.log(friends);
+	friends.forEach(function(friend){
+		console.log(friend)
+		renderFriends(friend);
+	})
+
+
+}
 
 
 
@@ -96,7 +163,6 @@ function clearMessages(){
 function allMessages(messageArray,roomName){
 	//Logic to get extract unique roomnames from the array and feed each to renderRoom function
 	data= messageArray;
-	console.log(messageArray);
 	var roomArray = messageArray.results.map(function(eachMessage){
 		return eachMessage.roomname;
 	});
@@ -110,7 +176,6 @@ function allMessages(messageArray,roomName){
 	//Logic to  filter messages from the array of Messages, based on roomName and feed to renderMessage function
 	function filterMessages(roomName){
 		var messageArray = data;  
-		console.log(messageArray);
 	var lobbyMessages= messageArray.results.filter(function(eachMessage){
 		return eachMessage.roomname === roomName;
 	});
@@ -120,13 +185,32 @@ function allMessages(messageArray,roomName){
 
 }
 
+
+
+
+
+//call .username in click function 
+
+var user;
+
 function renderMessage(message){
 	var innerHTML = "";
 	//console.log()	
 
 		var newMessage = $(/*"<tr><td><blockquote><p>*/ "</p></blockquote></td></tr>");
-		var userName = $('<small> @<a class ="username"> </a></small>')
-		userName.text(message.username);
+
+	
+		//var userName = $("<li class='link' data-user='+username+'>'" + userName + "'</li>'")
+		//var userName = $("</br><li class='link' data-user='+username'> <a href= '#' class ='username'> </a></li></small>")
+
+		
+		 var userName = $("</br><small id='username'><li id='links'><a href= '#' class ='username'> </a></li></small>")
+		 user = userName.text(message.username);
+		userName.attr(message.username);
+
+
+
+		//'<li class="link" data-user='+user+'>@' + user + '</li>');
 
 		newMessage.text(message.text).append(userName);
 		/*newMessage +'<small> @<a class ="username">' 
@@ -134,6 +218,17 @@ function renderMessage(message){
 	          
 		$("#chats").append(newMessage); 
 	}
+
+
+
+
+
+
+
+
+
+
+
 
 // done( function( data ) {
 //     var a = jQuery( '<a />' );
@@ -162,6 +257,32 @@ function renderRoom(roomName){
 
 		//  newRoom.addClass(roomName);
 		$("#roomSelect").append(newRoom);
+	}
+}
+
+
+function renderFriends(friendName){
+	//console.log(friendName);
+
+	if (friendName !== undefined) {
+		//var newRoom= "<li class='link' " + roomName +"><a href='#'>"+ roomName+ "</a></li>";
+		//var newRoom= "<li class='link'><a href='#'>"+ roomName+ "</a></li>";
+	 var newFriend= $("<li class='friendlink'><a href='#'> </a></li>");
+	  newFriend.text(friendName);
+
+// var a = jQuery( '<a />' );
+//     a.attr( 'href', data.url );
+//     a.text( data.title );
+ 
+//     jQuery( '#my-div' ).append( a );
+
+
+
+	if (!friends.includes(friendName)) {
+		$("#friendSelect").append(newFriend);
+	} 
+
+		//  newRoom.addClass(roomName);
 	}
 }
 
